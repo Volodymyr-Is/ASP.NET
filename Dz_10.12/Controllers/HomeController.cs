40742273e916a00692143b82f809da885765052d
Volodymyr-Is <isaienkovova05@gmail.com>
2024-12-16 16:04:29 +0200

    Add project files.

---
+++ Dz_10.12/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Dz_10._12.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dz_10._12.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dz_10.12/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dz_10._12.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
