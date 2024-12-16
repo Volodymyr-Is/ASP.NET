3cb7bc65ab61d31bc740143a975a1b64c710096e
Volodymyr-Is <144656592+Volodymyr-Is@users.noreply.github.com>
2024-12-16 15:59:56 +0200

    Delete Dz_12.12 directory

--- Dz_12.12/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Dz_16._12.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
